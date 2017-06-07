508298f03427a4322805636039b6833afad57f46
Pachj <Pachj@users.noreply.github.com>
2017-06-07 18:07:18 +0200

    comments added

--- src/index.js
+++ src/index.js
@@ -12,13 +12,20 @@ class App extends React.Component {
       enteredText: '',
     };
 
+    // binds this in handleInput to the class
     this.handleInput = this.handleInput.bind(this);
   }
 
+  /** stores  the input who the user enters in the #input textarea to the state
+   * @param event - the entered text
+   */
   handleInput(event) {
     this.setState({ enteredText: event.target.value });
   }
 
+  /** creates a markup from the text who the user had entered
+   * @return {Object} - the markup of the text
+   */
   createMarkup() {
     const marked = require('marked');
     return ({ __html: marked(this.state.enteredText) });
