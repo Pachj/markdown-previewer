7387068a122d0b00c917479e086ba9272482db5d
Pachj <Pachj@users.noreply.github.com>
2017-06-07 14:58:11 +0200

    App component added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 /**
  * Created by Henry on 07.06.17.
  */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
